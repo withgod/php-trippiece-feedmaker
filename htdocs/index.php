843a7313af37abcf6b19f3cafc85fda34332a06a
withgod <noname@withgod.jp>
2015-06-22 16:14:40 +0900

    added purge cache, changed rss library

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,11 +4,14 @@ require dirname(__FILE__) . '/../vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/rss/:token/:keyword', function ($token, $keyword) use ($app) {
+$app->get('/rss/:token/:keyword(/:purge)', function ($token, $keyword, $purge = 0) use ($app) {
     $expire    = 3600;
     $adapter   = new \Desarrolla2\Cache\Adapter\File(sys_get_temp_dir());
     $cache     = new \Desarrolla2\Cache\Cache($adapter);
-    $cache_key = 'tprss';
+    $cache_key = 'tprss_' . sha1($keyword);
+    if (!empty($purge)) {
+        $cache->delete($cache_key);
+    }
     $contents  = $cache->get($cache_key);
 
     $default = [
@@ -36,29 +39,46 @@ $app->get('/rss/:token/:keyword', function ($token, $keyword) use ($app) {
         if ($r->getStatusCode() == 200) {
             $body = (string)$r->getBody();
             $json = json_decode($body);
-            $atom = new \FeedWriter\ATOM();
-            $atom->setTitle('trippiece search result [' . $keyword . ']');
-            $atom->setLink('https://trippiece.com/');
-            $atom->setDate(new DateTime());
+
+            date_default_timezone_set('UTC');
+            $rss  = new \PicoFeed\Syndication\Rss20();
+            $rss->title = 'trippiece search result [' . $keyword . ']';
+            $rss->site_url = 'https://trippiece.com/';
+            $rss->feed_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+            $rss->author = ['name' => 'withgod', 'url' => 'http://withgod.hatenablog.com/', 'email' => 'noname@withgod.jp'];
             foreach ($json->results as $result) {
-                $item = $atom->createNewItem();
-                $item->setTitle($result->title);
-                $item->setLink('https://trippiece.com/plans/' . $result->id . '/');
-                $item->setDate($result->updated);
-                $item->setAuthor($result->producers[0]->user->full_name);
-                $item->setDescription($result->why);
-                //$item->setContent($result->notes[0]->text);
-                //$item->Enclosure();
-                $atom->addItem($item);
+                #2015-06-21T12:17:31
+                //var_dump(DateTime::createFromFormat('y-m-dTH:i:sZ', $result->updated, DateTimeZone::UTC));
+                $utc = new DateTimeZone('UTC');
+                $rss->items[] = [
+                    'title'     => $result->title,
+                    'updated'   => DateTime::createFromFormat('y-m-dTH:i:sZ', $result->updated, $utc),
+                    'published' => DateTime::createFromFormat('y-m-dTH:i:sZ', $result->created, $utc),
+                    'url'       => 'https://trippiece.com/plans/' . $result->id . '/',
+                    'author'    => [ 'name' => $result->producers[0]->user->full_name ],
+                    'summary'   => $result->why,
+                    //'content'   => $result->notes[0]->text,
+                    ];
             }
-            $contents = $atom->generateFeed();
+            $contents = $rss->execute();
             $cache->set($cache_key, $contents, $expire);
         } else {
             $app->halt(503, 'Service Temporarily Unavailable');
         }
     }
-    $app->response->headers->set('Content-type', 'application/atom+xml');
-    echo $contents;
+    $app->response->headers->set('Content-type', 'application/rss+xml');
+    //convert DATE_RFC822 to DATE_RFC2822
+    $replaced = preg_replace_callback(
+        '|<pubDate>([^<]+)|',
+        function ($matches) {
+            $date = $matches[1];
+            $arr  = explode(' ', $date);
+
+            return '<pubDate>' . $arr[0] . ' ' . $arr[1] . ' ' . $arr[2] . ' 20' . $arr[3] . ' ' . $arr[4] . ' ' . $arr[5];
+        },
+        $contents
+    );
+    echo $replaced;
 });
 $app->get('/', function () {
     print <<< EOT
